3dffc089b782466d473077804d49954bbdb23d5b
@ibrakhim-1825 <frontend1825@gmail.com>
2024-03-16 05:59:50 +0300

    git commit -m "other"

--- main.js
+++ main.js
@@ -102,19 +102,116 @@
 
 // 11-masala
 
-function boluvchilariniTop(raqam) {
-    let boluvchilar = [];
-    for (let i = 1; i <= raqam; i++) {
-        if (raqam % i === 0) {
-            boluvchilar.push(i);
-        }
-    }
-    return boluvchilar;
-}
+// function boluvchilariniTop(raqam) {
+//     let boluvchilar = [];
+//     for (let i = 1; i <= raqam; i++) {
+//         if (raqam % i === 0) {
+//             boluvchilar.push(i);
+//         }
+//     }
+//     return boluvchilar;
+// }
 
-let raqam = 25;
-let boluvchilar = boluvchilariniTop(raqam);
-console.log(raqam + " raqami boluvchilari: " + boluvchilar);
+// let raqam = 25;
+// let boluvchilar = boluvchilariniTop(raqam);
+// console.log(raqam + " raqami boluvchilari: " + boluvchilar);
+
+
+// CODINGBAT
+
+// masala1
+// const y = (str) => {
+//     result = <<${str}>>
+//     return result;
+// }
+// result = y(prompt());
+// console.log(result)
+
+// masala2
+// const x = (name) => {
+//     return Hello ${name}!
+// }
+// result = x(prompt());
+// console.log(result)
+
+
+
+// masala3 
+function x(str) {
+    result = str.substring(0, str.length / 2);
+    if (str.length % 2 == 1) {
+        return "Harflarning soni toq !!! ==> " + result;
+    }
+    return result;
+}
+// result = x(prompt())
+// console.log("Natija ==>", result);
+
+
+// masala4
+// const x = (str, front) => {
+//     if (front) {
+//         return str.charAt(0)
+//     } else {
+//         return str.slice(str.length - 1, str.length)
+//     }
+// }
+// true or false
+// result = x(prompt(), false)
+// console.log(result);
+
+// masala5
+// const x = (str1, str2) => {
+//     return str1.substring(1) + str2.substring(1);
+// }
+// result = x(prompt(), prompt())
+// console.log(result);
+
+
+
+// masala6 
+const x = (str) => {
+    return str.endsWith("ly")
+}
+// result = x(prompt())
+// console.log(result);
+
+
+// masala7
+// const x = (str1, str2) => {
+//     if (str1.length === 0) {
+//         str1 = '@';
+//     }
+//     if (str2.length === 0) {
+//         str2 = '@';
+//     }
+//     return str1.charAt(0) + str2.charAt(str2.length - 1);
+// }
+// result = x(prompt(), prompt());
+// console.log(result);
+
+// masala8
+// const x = (str) => {
+//     let index = Math.floor((str.length - 3) / 2);
+//     return str.substring(index, index + 3);
+// }
+// result = x(prompt());
+// console.log(result);
+
+
+
+// masala9 
+// const x = (str) => {
+//     if (str.slice(0, 4) == "blue") {
+//         return str;
+//     } else if (str.slice(0, 3) == "red") {
+//         return str;
+//     } else {
+//         return "";
+//     }
+// }
+// result = x(prompt())
+// console.log(result);
 
 
 
